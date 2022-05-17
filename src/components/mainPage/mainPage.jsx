5143c0f49c732a40b37997a9e40acbdc1637b9fb
Alexey Khlebnikov <khlebnikov@team.picom.ru>
2022-05-17 18:19:56 +0400

    стартовый шаблон приложения

---
+++ src/components/mainPage/mainPage.jsx
@@ -0,0 +1,14 @@
+import './mainPage.css';
+
+
+const Page = () => {
+    return <>
+        <div className="mainPage">
+            <div className="mainPage__container">
+                123124
+            </div>
+        </div>
+    </>
+}
+
+export default Page;
